4419c61d11c76b32a72a7aa7ff0c4d6a45a3c063
padma chockalingam <jmcpadma@gmail.com>
2017-12-11 17:58:10 +0530

    second

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+
 var todoController = require('./controllers/todocontroller');
 
 var app = express();
--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,4 +1,20 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to mongodb
+mongoose.connect('mongodb://test:test@ds129966.mlab.com:29966/nodetodotest');
+
+// create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+// create a model for db
+var Todo = mongoose.model('todos', todoSchema);
+var itemOne = Todo({item:'buy flowers'}).save(function(err){
+    if(err)throw err;
+    console.log('item saved');
+});
 
 var data = [{item:'get milk'},{item: 'walk dog'},{item:'write some coding'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
